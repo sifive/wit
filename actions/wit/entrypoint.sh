20067aaa6c6775133d2cfbefeb477a1c639aec00
Nathaniel Graff <nathaniel.graff@sifive.com>
2020-02-19 08:08:21 -0800

    Add support for HTTP Basic Authentication (#208)
    Adds the options:
     - `http_auth_username`
     - `http_auth_token`
    for use with HTTP Basic Authentication.
    
    The default value of `http_auth_username` is 'x-access-token', which is
    the useranme accepted by GitHub for authenication with GitHub Personal
    Access Tokens.
    
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- actions/wit/entrypoint.sh
+++ actions/wit/entrypoint.sh
@@ -5,7 +5,14 @@ set -euxo pipefail
 : ${INPUT_COMMAND:?'The `command` input argument must be set'}
 
 if [[ -n "$INPUT_FORCE_GITHUB_HTTPS" ]]; then
-  git config --global url.'https://github.com/'.insteadOf 'git@github.com:'
+  # Don't echo the token
+  set +x
+  if [[ -n "$INPUT_HTTP_AUTH_TOKEN" ]]; then
+    git config --global url."https://${INPUT_HTTP_AUTH_USERNAME}:${INPUT_HTTP_AUTH_TOKEN}@github.com/".insteadOf 'git@github.com:'
+  else
+    git config --global url.'https://github.com/'.insteadOf 'git@github.com:'
+  fi
+  set -x
 fi
 
 exec wit "$INPUT_COMMAND" $INPUT_ARGUMENTS
